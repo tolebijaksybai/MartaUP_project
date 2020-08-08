a3949b9c30b11d7cde807b14b43a93fc52faac28
Tolebi Zhaksybay <52714747+tolebijaksybai@users.noreply.github.com>
2020-08-08 23:39:15 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$(function(){
+    $('.header-slider').slick({
+        arrows: false,
+        vertical: true,
+        dots: true,
+        dotsClass: 'header-dots',
+        autoplay: 2000
+        // fade: true,
+    });
+
+    $('.menu__btn').on('click', function(){
+        $('.menu__list').slideToggle();
+    });
+
+
+})
